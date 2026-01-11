42bac30df9a712e1af714f32ca78f6c0ddfd901d
OmarDevSec <omarsecure30@gmail.com>
2026-01-10 20:07:39 -0800

    Notes on time complexity and Big O notation

---
+++ Complexity.py
@@ -0,0 +1,18 @@
+# Complexity and Big O notation: Complexity of an algorithm is a measure of the 
+# amount of time and/or space required by an alogortithm to process and input of 
+# a given size e.g 'N'. Unless otherwise stated, the temr complexity always refers
+# to the worst-case complexity (i.e the highest possible time/space taken by the program
+# /algorith to process an input.)
+
+# In the case of a linear search: 
+# 1.) The time complexit of the algorithm is 'cN' for some fixed constant 'c' that 
+# depends on the number of operations we perfomr in each iteration and the time 
+# taken to execute a statement. Time complexity is sometimes also called the running
+# time of the algorithm.
+
+# 2.) The space complexity is some constant 'c' (independant of N), since we just
+# need a single variable postiion to iterate through the array, and it occupies
+#  a constant space in the computer's memory (RAM)
+
+# Thus the time complexity of linear search is O(N) and its space complexity is O(1)
+
