9e9c8df46a65e2fa9d5702893aaea5c8e2711852
OmarDevSec <omarsecure30@gmail.com>
2026-01-13 16:08:38 -0800

    completed leetcode medium Binary Search problem, troubleshooting and understanding edge case erros.

--- 3stepBS.py
+++ 3stepBS.py
@@ -29,3 +29,31 @@ def binary_search(arr, target):
 # 1.) Set left and right pointers to boundaries of the search space
 # 2.) Set return value to left or left -1
 # 3.) Define the condition that will check if midpoint is valid (maybe a function)
+
+
+# More practice :)
+
+# 1. left = 0, right = len(nums)
+# 2. return left
+# 3. use arr[mid] > arr[mid+1]
+
+class Solution:
+    def findPeakElement(self, nums:List[int]) -> int:
+        # Edge case: if only one element, it's a peak
+        if len(nums) == 1:
+            return 0
+
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = l + (r-l) //2
+
+            if nums[mid] > nums [mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l # return needs to be outside of the loop, now the leetcode
+    
+    # problem works perfectly.
